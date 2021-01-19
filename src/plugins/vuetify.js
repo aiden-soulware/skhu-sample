94cf75f9634003f3094cce5c69c856ba625e5469
aiden <icayenju@gmail.com>
2021-01-20 00:46:27 +0900

    *

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,10 +1,20 @@
 import Vue from 'vue';
 import Vuetify from 'vuetify/lib/framework';
+import colors from 'vuetify/lib/util/colors'
 
 Vue.use(Vuetify);
 
 export default new Vuetify({
   icons: {
     iconfont: 'mdi', // default - only for display purposes
+  },
+  theme: {
+    themes: {
+      light: {
+        primary: colors.indigo.base , // #E53935
+        secondary: colors.red.darken1, // #FFCDD2
+        accent: colors.red.lighten4, // #3F51B5
+      },
+    },
   },
 });
