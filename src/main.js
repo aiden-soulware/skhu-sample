bab73c389463062473dcdf0ff1849bf478f00eee
aiden <icayenju@gmail.com>
2021-01-20 00:34:09 +0900

    * vuetify

--- src/main.js
+++ src/main.js
@@ -1,12 +1,18 @@
+// Polyfills
+import 'core-js/stable'
+import 'regenerator-runtime/runtime'
+
 import Vue from 'vue'
-import App from './App.vue'
+import App from './app.vue'
 import router from './router'
 import store from './store'
+import vuetify from './plugins/vuetify';
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
+  vuetify,
   render: h => h(App)
 }).$mount('#app')
