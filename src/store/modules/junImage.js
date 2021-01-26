e387706b7582c40ecf177259722b017100b702e8
jhj960918 <jhj960918@gmail.com>
2021-01-26 16:40:44 +0900

    jun : update

--- src/store/modules/junImage.js
+++ src/store/modules/junImage.js
@@ -1,12 +1,47 @@
+import axios from "axios";
 const state = {
   items: [],
+  url : [
+    "https://images.unsplash.com/photo-1610048899906-d8f64bc45464?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format",
+    "https://images.unsplash.com/photo-1610048899906-d8f64bc45464?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format",
+    "https://s3.ap-northeast-2.amazonaws.com/elasticbeanstalk-ap-northeast-2-176213403491/media/magazine_img/magazine_319/112_%E1%84%8A%E1%85%A5%E1%86%B7%E1%84%82%E1%85%A6%E1%84%8B%E1%85%B5%E1%86%AF.jpg",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTutO1nmrYhFBcRcdUQPaOPp-HAiJrnzH-nuA&usqp=CAU",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRi7OhfW0BhZzL3D4Swp_A7GQPwJE2OhsjXYw&usqp=CAU",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSa2UDtN4kxCCQYnQ4_yoceVrbDOnEED7aHig&usqp=CAU",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS-nct0eVHBKTlAJkfX5suKzzOtOOrC0p2c7A&usqp=CAU",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTJeIhe-_I6rzcN1Oa4xPvttkUNcrYEv8EP2Q&usqp=CAU",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSkPId4Hjm02Db3jqaXTn6UpzZzVfJB7I8lUw&usqp=CAU",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT1cCPc3QZCzUKtoh9rdC5WTfLegbi26jvMmA&usqp=CAU",
+  ],
 };
-const getters = {};
+const getters = {
+  
+};
 const actions = {
   //비즈니스로직이 들어 갈 수 있음 ex) password 검사 비동기 처리 같은 로직 처리 후
   //commit 을 사용해 mutations에 전달
   getImageItem: ({ commit }, payload) => {
     commit("imageItem", payload);
+  },
+  getImageBase64 : (url)=> {
+    return new Promise((resolve, reject) => {
+      axios
+        .get(url, {
+          responseType: "arraybuffer",
+        })
+        .then(
+          (response) => {
+            let temp = Buffer.from(response.data, "binary").toString(
+              "base64"
+            );
+            resolve(temp);
+          },
+          (err) => {
+            console.log(err);
+            reject(err);
+          }
+        );
+    });
   },
 };
 
--- src/views/jun/ImagePromise/ImagePrint.vue
+++ src/views/jun/ImagePromise/ImagePrint.vue
@@ -10,78 +10,50 @@
   </div>
 </template>
 <script>
-import axios from "axios";
+
 import { mapActions, mapState } from "vuex";
 export default {
   data() {
     return {
-      imageData: null,
       uploadPercentage: 0,
-      // items: [],
       promiseArray: [],
     };
   },
   computed: {
     ...mapState({
       items: (state) => state.junImage.items,
+      url:(state) => state.junImage.url,
+
     }),
   },
 
   methods: {
     ...mapActions({
-      getImageItem: "junImage/getImageItem",
+      getImageItem: 'junImage/getImageItem',
+      getImageBase64: 'junImage/getImageBase64',
     }),
 
     getData() {
-      let url = [
-        "https://images.unsplash.com/photo-1610048899906-d8f64bc45464?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format",
-        "https://images.unsplash.com/photo-1610048899906-d8f64bc45464?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format",
-        "https://s3.ap-northeast-2.amazonaws.com/elasticbeanstalk-ap-northeast-2-176213403491/media/magazine_img/magazine_319/112_%E1%84%8A%E1%85%A5%E1%86%B7%E1%84%82%E1%85%A6%E1%84%8B%E1%85%B5%E1%86%AF.jpg",
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTutO1nmrYhFBcRcdUQPaOPp-HAiJrnzH-nuA&usqp=CAU",
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRi7OhfW0BhZzL3D4Swp_A7GQPwJE2OhsjXYw&usqp=CAU",
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSa2UDtN4kxCCQYnQ4_yoceVrbDOnEED7aHig&usqp=CAU",
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS-nct0eVHBKTlAJkfX5suKzzOtOOrC0p2c7A&usqp=CAU",
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTJeIhe-_I6rzcN1Oa4xPvttkUNcrYEv8EP2Q&usqp=CAU",
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSkPId4Hjm02Db3jqaXTn6UpzZzVfJB7I8lUw&usqp=CAU",
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT1cCPc3QZCzUKtoh9rdC5WTfLegbi26jvMmA&usqp=CAU",
-      ];
-
+      
       let temp;
-      for (var i = 0; i < url.length; i++) {
-        temp = this.getImageBase64(url[i]);
+      for (var i = 0; i < this.url.length; i++) {
+        temp = this.getImageBase64(this.url[i]);//actions call
         this.promiseArray.push(temp);
 
         temp.then((data) => {
           this.getImageItem("data:image/png;base64," + data);
-          this.uploadPercentage += 100 / url.length;
+          this.uploadPercentage += 100 / this.url.length;
         });
       }
-
+                         
       Promise.all(this.promiseArray).then(function() {
         alert("이미지 불러오기 완료");
+        
       });
     },
 
-    getImageBase64(url) {
-      return new Promise((resolve, reject) => {
-        axios
-          .get(url, {
-            responseType: "arraybuffer",
-          })
-          .then(
-            (response) => {
-              let temp = Buffer.from(response.data, "binary").toString(
-                "base64"
-              );
-              resolve(temp);
-            },
-            (err) => {
-              console.log(err);
-              reject(err);
-            }
-          );
-      });
-    },
+
+    
   },
 };
 </script>
